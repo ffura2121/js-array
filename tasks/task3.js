191be7bd93e78bc31e941c78e3e393069ca49f12
ffura2121 <153360106+ffura2121@users.noreply.github.com>
2024-10-12 20:00:21 +0300

    Update task3.js

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,6 +1,24 @@
 // З масива людей(people) Вам потрібно знайти людину за іменем(name)
 function findByName(people, name) {
-  // Ваш код
+  for (let i = 0; i < people.length; i++) {
+    if (people[i].name === name) {
+      return people[i];
+    }
+  }
+  return null;
+}
+
+const people = [
+  { name: "John", age: 20 },
+  { name: "Jane", age: 22 },
+  { name: "Jim", age: 19 },
+  { name: "Jill", age: 21 },
+  { name: "Jack", age: 23 },
+];
+
+
+console.log(findByName(people, "Jane")); 
+module.exports = findByName;
 }
 
 module.exports = findByName;
